1dd0fe16ae9a56cace8a353ed34727a58c0e1b88
sblanky <pcxtsbl@nottingham.ac.uk>
2022-01-13 11:26:43 +0000

    made installable via pip install .

---
+++ pyPUC/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyPUC
+
+Brute force determination of optimum pore size range for gas uptake from experimental data
+"""
+
+__version__ = "0.1.0"
+__author__ = "L. Scott Blankenship"
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyPUC',
+    version='0.1.0',
+    url='https://github.com/sblanky/pyPUC',
+    author='L. Scott Blankenship',
+    author_email='pcxtsbl@nottingham.ac.uk',
+    license='MIT',
+    packages=['pyPUC'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
