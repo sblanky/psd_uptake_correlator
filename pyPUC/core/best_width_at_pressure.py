cdf1960a148beec677319e5b741c41da646812e5
sblanky <pcxtsbl@nottingham.ac.uk>
2022-01-27 10:59:03 +0000

    plot_correlations()

--- pyPUC/core/best_width_at_pressure.py
+++ pyPUC/core/best_width_at_pressure.py
@@ -152,3 +152,26 @@ def correlation_requirements(correlation_df,
                   ''')
 
     return correlation_df
+
+def plot_correlations(df, results_path):
+    for index, row in df.iterrows(): 
+        f, ax = plt.subplots(nrows=1, ncols=1,
+                             figsize=(8, 8), dpi=96)
+
+        x = df.loc[index, 'x']
+        y = df.loc[index, 'y']
+
+        ax.scatter(x, y,
+                   ec='k', fc='none')
+
+        x_line = np.linspace(min(x), max(x), 100)
+        y_line = df.loc[index, 'm'] * x_line + df.loc[index, 'c']
+        ax.plot(x_line, y_line, color='k')
+        ax.annotate(f"$r^2$ = {format(df.loc[index, 'r_sq'], '.2f')}", 
+                    xy=(0.05, 0.95), xycoords='axes fraction')
+        path = f"{results_path}/plots/correlations/{df.loc[index, 'p']}/"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        name = f"{df.loc[index, 'wmin']}_{df.loc[index, 'wmax']}.png"
+        f.savefig(f"{path}{name}")
+        plt.close(f)
